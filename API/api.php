c60753b94a58598556aa6d5053fa7f7d768cf3a2
vvalcarcell <valcarcellvalentina@gmail.com>
2021-08-30 16:40:11 +0200

    milestone1

--- API/api.php
+++ API/api.php
@@ -1,4 +1,4 @@
 <?php
-    require_once __DIR__ . "/database/database.php";
+    require_once __DIR__ . "/../database/database.php";
     header('Content-Type: application/json');
     echo json_encode($database);
--- index.php
+++ index.php
@@ -4,19 +4,38 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Roboto&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="style.css">
     <title>Document</title>
 </head>
 <body>
-    
-    <?php
-    
-        require_once __DIR__ . "/database.php";
 
-        foreach($database as $item) {
-            var_dump($item);
-        }
+    <div class="container-fluid">
+    <header>    
+        <img src="./img/spotify-logo.png" alt="" />
+    </header>
+    
+    <main>
+        <?php
+        
+            require_once __DIR__ . "/database/database.php";
 
-    ?>
+            foreach($database as $item) {
+                echo "<div class=\"album-element\">
+                            <img src=\"{$item[poster]}\"  />
+                            <div class=\"title\">{$item[title]}</div>
+                            <div class=\"info\">{$item[author]}</div>
+                            <div class=\"info\">{$item[year]}</div>
+                        </div>";
+            };
+
+        ?>
+    </main>
+    </div>
+
+
     
 
 
