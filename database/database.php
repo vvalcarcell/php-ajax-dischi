320430a7029ece5aa073b1c2859cb14b29fd459b
vvalcarcell <valcarcellvalentina@gmail.com>
2021-08-30 16:57:34 +0200

    milestone1 completa

--- database/database.php
+++ database/database.php
@@ -29,12 +29,40 @@
             'title' => 'Brave new World',
             'author' => 'Iron Maiden',
             'year' => 2000,
-            'poster' => 'https://i.ebayimg.com/images/g/1GwAAOSwFytaKr7l/s-l300.jpg'
+            'poster' => 'https://images-na.ssl-images-amazon.com/images/I/810nSIQOLiL._SY355_.jpg'
         ],
         [
             'title' => 'One more car, one more rider',
             'author' => 'Eric Clapton',
             'year' => 2002,
             'poster' => 'https://images-na.ssl-images-amazon.com/images/I/81MDAIdh78L._SY355_.jpg'
+        ],
+        [
+            "poster"=> "https://images-na.ssl-images-amazon.com/images/I/51rggtPgmRL.jpg",
+            "title"=> "Made in Japan",
+            "author"=> "Deep Purple",
+            "genre"=> "Rock",
+            "year"=> "1972"
+        ],
+        [
+            "poster"=> "https://images-na.ssl-images-amazon.com/images/I/81r3FVfNG3L._SY355_.jpg",
+            "title"=> "And Justice for All",
+            "author"=> "Metallica",
+            "genre"=> "Metal",
+            "year"=> "1988"
+        ],
+        [
+            "poster"=> "https://img.discogs.com/KOBsqQwKiNKH-q927oHMyVdDzSo=/fit-in/596x596/filters:strip_icc():format(jpeg):mode_rgb():quality(90)/discogs-images/R-6406665-1418464475-6120.jpeg.jpg",
+            "title"=> "Hard Wired",
+            "author"=> "Dave Weckl",
+            "genre"=> "Jazz",
+            "year"=> "1994"
+        ],
+        [
+            "poster"=> "https://m.media-amazon.com/images/I/71K9CbNZPsL._SS500_.jpg",
+            "title"=> "Bad",
+            "author"=> "Michael Jacjson",
+            "genre"=> "Pop",
+            "year"=> "1987"
         ]
     ];
